3a3a7052dc30af54cf5d0d044ba773f5a236e7b2
Bruno Mayer <bs.mayer@hotmail.com>
2019-11-02 00:04:07 +0000

    checking for undefined or null values in the path

--- src/lib/HttpClientResponse.ts
+++ src/lib/HttpClientResponse.ts
@@ -1,20 +1,20 @@
-import HttpClientConfiguration from './HttpClientConfiguration'
-import AxiosService from './services/AxiosService'
-import RequestService from './services/RequestService'
-import FetchService from './services/FetchService'
-import GotService from './services/GotService'
-import HttpMethod from './constants/HttpMethod'
+import HttpClientConfiguration from './HttpClientConfiguration';
+import AxiosService from './services/AxiosService';
+import RequestService from './services/RequestService';
+import FetchService from './services/FetchService';
+import GotService from './services/GotService';
+import HttpMethod from './constants/HttpMethod';
 
 export default class HttpClientResponse {
-  private baseUrl: string
-  private paths: string[]
-  private params: any
-  private method: HttpMethod
-  private body: any
-  private headers: any
-  private configuration: HttpClientConfiguration
+  private baseUrl: string;
+  private paths: string[];
+  private params: any;
+  private method: HttpMethod;
+  private body: any;
+  private headers: any;
+  private configuration: HttpClientConfiguration;
 
-  constructor (
+  constructor(
     baseUrl: string,
     paths: string[],
     params: any[],
@@ -23,29 +23,31 @@ export default class HttpClientResponse {
     headers: any,
     configuration: HttpClientConfiguration
   ) {
-    this.baseUrl = baseUrl
-    this.paths = paths
-    this.params = params
-    this.method = method
-    this.body = body
-    this.headers = headers
-    this.configuration = configuration
+    this.baseUrl = baseUrl;
+    this.paths = paths;
+    this.params = params;
+    this.method = method;
+    this.body = body;
+    this.headers = headers;
+    this.configuration = configuration;
   }
 
-  public async getResponse<T> (): Promise<T> {
+  public async getResponse<T>(): Promise<T> {
     if (this.baseUrl.endsWith('/')) {
-      this.baseUrl = this.baseUrl.substring(0, this.baseUrl.length - 1)
+      this.baseUrl = this.baseUrl.substring(0, this.baseUrl.length - 1);
     }
 
     const finalPath = this.paths.reduce((path, curr) => {
+      if (!curr) throw Error('There is a problem with your path, received undefined value');
+
       if (!curr.startsWith('/')) {
-        curr += '/'
+        curr += '/';
       }
       if (curr.endsWith('/')) {
-        curr = curr.substring(0, curr.length - 1)
+        curr = curr.substring(0, curr.length - 1);
       }
-      return path + '/' + curr
-    }, '')
+      return path + '/' + curr;
+    }, '');
 
     const request = {
       baseUrl: this.baseUrl,
@@ -54,78 +56,72 @@ export default class HttpClientResponse {
       payload: this.body,
       headers: this.headers,
       params: this.params,
-    }
+    };
 
-    return this.fetchRetry<T>(async (): Promise<T> => {
-      let responseBody
-      let originalResponse
+    return this.fetchRetry<T>(
+      async (): Promise<T> => {
+        let responseBody;
+        let originalResponse;
 
-      if (this.configuration.isAxios()) {
-        originalResponse = await AxiosService
-          .create(this.configuration.client)
-          .makeRequest(request)
+        if (this.configuration.isAxios()) {
+          originalResponse = await AxiosService.create(this.configuration.client).makeRequest(request);
 
-        responseBody = originalResponse.data
-      } else if (this.configuration.isRequest()) {
-        originalResponse = await RequestService
-          .create(this.configuration.client)
-          .makeRequest(request)
+          responseBody = originalResponse.data;
+        } else if (this.configuration.isRequest()) {
+          originalResponse = await RequestService.create(this.configuration.client).makeRequest(request);
 
-        responseBody = JSON.parse(originalResponse.body)
-      } else if (this.configuration.isGot()) {
-        originalResponse = await GotService
-          .create(this.configuration.client)
-          .makeRequest(request)
+          responseBody = JSON.parse(originalResponse.body);
+        } else if (this.configuration.isGot()) {
+          originalResponse = await GotService.create(this.configuration.client).makeRequest(request);
 
-        responseBody = JSON.parse(originalResponse.body)
-      } else {
-        originalResponse = await FetchService
-          .makeRequest(request)
-        responseBody = JSON.parse(originalResponse.body)
+          responseBody = JSON.parse(originalResponse.body);
+        } else {
+          originalResponse = await FetchService.makeRequest(request);
+          responseBody = JSON.parse(originalResponse.body);
+        }
+
+        if (this.configuration.interceptors) {
+          const resolvedResponse = this.configuration.interceptors.applyResponseInterceptor(
+            responseBody,
+            originalResponse
+          );
+          if (resolvedResponse) return resolvedResponse as T;
+        }
+
+        return responseBody as T;
       }
-
+    ).catch(err => {
       if (this.configuration.interceptors) {
-        const resolvedResponse = this.configuration.interceptors.applyResponseInterceptor(responseBody, originalResponse)
-        if (resolvedResponse)
-          return resolvedResponse as T
+        const resolvedError = this.configuration.interceptors.applyErrorInterceptor(err.originalError || err);
+        if (resolvedError) return resolvedError;
       }
 
-      return responseBody as T
-    }).catch(err => {
-      if (this.configuration.interceptors) {
-        const resolvedError = this.configuration.interceptors.applyErrorInterceptor(err.originalError || err)
-        if (resolvedError)
-          return resolvedError
-      }
-
-      throw err.originalError || err
-    })
+      throw err.originalError || err;
+    });
   }
 
-  private fetchRetry<T> (request: () => Promise<T>, attempt = 1): Promise<T> {
+  private fetchRetry<T>(request: () => Promise<T>, attempt = 1): Promise<T> {
     return new Promise<T>((resolve, reject): void => {
       request()
         .then(response => resolve(response))
         .catch(err => {
-          if (!this.configuration.retry)
-            return reject(err)
+          if (!this.configuration.retry) return reject(err);
 
-          if (attempt >= this.configuration.retry.getAttempts())
-            return reject(err)
+          if (attempt >= this.configuration.retry.getAttempts()) return reject(err);
 
           if (err.statusCode && !this.configuration.retry.checkIfShouldRetry(err.statusCode))
-            return reject(err.originalError)
+            return reject(err.originalError);
 
           const interval = this.configuration.retry.isExponential()
             ? (2 ^ attempt) * this.configuration.retry.getInterval()
-            : this.configuration.retry.getInterval()
+            : this.configuration.retry.getInterval();
 
           setTimeout(() => {
             return this.fetchRetry(request, attempt + 1)
               .then(res => resolve(res))
-              .catch(err => reject(err))
-          }, interval)
-        })
-    })
+              .catch(err => reject(err));
+          }, interval);
+        });
+    });
   }
 }
